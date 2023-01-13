0b782cf4a4b5aa502e72276adf711d7d507a46d1
Claudia Meadows <contact@claudiameadows.dev>
2023-01-12 18:27:26 -0800

    Fix a soundness hole
    Just like `Mutex<T>` is only `Sync` if `T` is `Send`, `SyncWrapper<T>` should contain the same constraints.

--- src/lib.rs
+++ src/lib.rs
@@ -164,7 +164,7 @@ impl<T> SyncWrapper<T> {
 
 // this is safe because the only operations permitted on this data structure require exclusive
 // access or ownership
-unsafe impl<T> Sync for SyncWrapper<T> {}
+unsafe impl<T: Send> Sync for SyncWrapper<T> {}
 
 impl<T> Debug for SyncWrapper<T> {
     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
